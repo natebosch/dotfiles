21e3135be9dbfc15baa4b970eb18537bde93d806
Nathan Bosch <nbosch1@gmail.com>
2016-01-07 15:59:13 -0800

    Keep the TMUX env variable, clear only for nesting
    fzf uses this variable to know when it can take advantage of tmux to do
    fuzzy finding in a new pane. Rather than blow this away from the start,
    only clear it immediately before firing up a nested session

--- zshrc.d/tmux.zsh
+++ zshrc.d/tmux.zsh
@@ -1,18 +1,10 @@
-# Allow nesting but keep a record
-# Useful for 's' script
-if [[ -n $TMUX ]] then
-    export TMUX_ENV_BACKUP=$TMUX
-    export IN_TMUX=true
-    unset TMUX # allow nesting
-fi
-
 alias here='tmux set-option default-path "$PWD"'
 
 s() {
   session=${1:-0} # Default to session named '0'
 
   # Check TMUX_SESSION to check for a nested loop session
-  if [[ -n $IN_TMUX ]] then
+  if [[ -n $TMUX ]] then
     nestedsessions=$(tmux show-environment | grep TMUX_SESSION | cut -d'=' -f2)
     nestedsessions=(${(s/ /)nestedsessions})
     if [[ ${nestedsessions[(i)$session]} -le ${#nestedsessions} ]] then
@@ -22,7 +14,7 @@ s() {
   fi
   new_session=$nestedsessions
   new_session+=($session)
-  if [[ -n $IN_TMUX ]]
+  if [[ -n $TMUX ]]
   then
     tmux rename-window $session
   fi
@@ -30,14 +22,14 @@ s() {
     #session exists, attach to it and detach other clients
     TMUX_SESSION=$new_session tmux -2 a -t $session -d
   else
-    #create the session
-    tmux -2 new -s $session -d
+    #create the session, force tmux to allow nesting
+    TMUX="" tmux -2 new -s $session -d
     if [[ $session == "0" ]]
     then
       tmux source-file $DOTDIR/.tmux-root.conf
     fi
-    tmux set-option -t $session default-path "$PWD"
-    TMUX_SESSION=$new_session tmux -2 a -t $session -d
+    TMUX="" tmux set-option -t $session default-path "$PWD"
+    TMUX_SESSION=$new_session TMUX="" tmux -2 a -t $session -d
   fi
   unset new_session,session,nestedsessions
 }
