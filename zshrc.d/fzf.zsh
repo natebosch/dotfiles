94e82ddc0af726fce3eb4733c5deb1a1bde770d0
Nathan Bosch <nbosch1@gmail.com>
2016-01-07 19:34:04 -0800

    Use local definition of find_files if it exists

--- zshrc.d/fzf.zsh
+++ zshrc.d/fzf.zsh
@@ -2,7 +2,7 @@ if [[ -f ~/.fzf.zsh ]] ; then
   source ~/.fzf.zsh
 
   if (( $+commands[ag] )) ; then
-    export FZF_DEFAULT_COMMAND="ag -l --nocolor --nogroup --depth 10 -g ''"
+    export FZF_DEFAULT_COMMAND="find_files || ag -l --nocolor --nogroup --depth 10 -g ''"
   fi
 else
   bindkey "^r" history-incremental-search-backward
