ea3eecf37a8cae987927fb16581d0eced75f8e61
Nathan Bosch <nbosch1@gmail.com>
2016-01-14 15:16:15 -0800

    Simpler tmux session handling
    - Don't bother trying to nest sessions, switch to it instead
    - Pull up fzf session picker if no argument was given
    - Don't bother with the special '0' shell session

--- zshrc.d/tmux.zsh
+++ zshrc.d/tmux.zsh
@@ -1,36 +1,27 @@
 alias here='tmux set-option default-path "$PWD"'
 
 s() {
-  session=${1:-0} # Default to session named '0'
-
-  # Check TMUX_SESSION to check for a nested loop session
-  if [[ -n $TMUX ]] then
-    nestedsessions=$(tmux show-environment | grep TMUX_SESSION | cut -d'=' -f2)
-    nestedsessions=(${(s/ /)nestedsessions})
-    if [[ ${nestedsessions[(i)$session]} -le ${#nestedsessions} ]] then
-      echo "Already in session $session"
-      exit -1
-    fi
-  fi
-  new_session=$nestedsessions
-  new_session+=($session)
-  if [[ -n $TMUX ]]
-  then
-    tmux rename-window $session
-  fi
-  if tmux has -t $session; then
-    #session exists, attach to it and detach other clients
-    TMUX_SESSION=$new_session tmux -2 a -t $session -d
+  local session
+  if (($+1)) then
+    session=$1
   else
-    #create the session, force tmux to allow nesting
-    TMUX="" tmux -2 new -s $session -d
-    if [[ $session == "0" ]]
-    then
-      tmux source-file $DOTDIR/.tmux-root.conf
-    fi
-    TMUX="" tmux set-option -t $session default-path "$PWD"
-    TMUX_SESSION=$new_session TMUX="" tmux -2 a -t $session -d
+    session=$(tmux list-sessions -F "#{session_name}" | fzf --exit-0)
   fi
-  unset new_session,session,nestedsessions
+
+  if [ -z "$session" ]; then
+    echo "No session specified!"
+    return -1
+  fi
+
+  if (! tmux has -t $session) then
+    TMUX="" tmux -2 new -s $session -d
+    TMUX="" tmux set-option -t $session default-path "$PWD"
+  fi
+
+  if [[ -n $TMUX ]] then
+    tmux switch-client -t "$session"
+  else
+    tmux -2 a -t $session -d
+  fi
 }
 compdef '_arguments "1:tmux session:($(tmux ls -F \#\{session_name\}))"' s
