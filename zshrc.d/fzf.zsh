07c40203c071f1bd6e115ecbdd4bc74e35114480
Nathan Bosch <nbosch1@gmail.com>
2016-01-14 11:56:06 -0800

    Use exact match mode by default in fzf

--- zshrc.d/fzf.zsh
+++ zshrc.d/fzf.zsh
@@ -1,6 +1,8 @@
 if [[ -f ~/.fzf.zsh ]] ; then
   source ~/.fzf.zsh
 
+  export FZF_DEFAULT_OPTS="--exact"
+
   if (( $+commands[ag] )) ; then
     export FZF_DEFAULT_COMMAND="find_files || ag -l --nocolor --nogroup --depth 10 -g ''"
   fi
