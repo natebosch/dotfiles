1b6e34d035e25146bee9459693ae5f8938586952
Nate Bosch <nbosch1@gmail.com>
2016-03-12 11:11:15 -0800

    Replace tmux default-path option
    Tmux dropped support for the default-path option - instead suggestion
    to bind a key to create-window with the -c flag set. This works well
    for creating new windows or panes with the same path as the current
    window, but I prefer each session to have a default-path so no matter
    what window I am in a new window behaves the same.
    
    Add a function to
    - Rename the session to get it out of the way
    - Create a new session with the same name. Creating the session sets
      the directory for new windows to $PWD
    - Move all windows over to the new session and switch to it
    
    Works best with set-option -g renumber-windows to keep window indexes
    the same in the new session

--- zshrc.d/tmux.zsh
+++ zshrc.d/tmux.zsh
@@ -1,5 +1,3 @@
-alias here='tmux set-option default-path "$PWD"'
-
 s() {
   local session
   if (($+1)) then
@@ -24,3 +22,17 @@ s() {
   fi
 }
 compdef '_arguments "1:tmux session:($(tmux ls -F \#\{session_name\}))"' s
+
+change_tmux_pwd() {
+  local session_name=$(tmux display-message -p '#S')
+  local tmp_session_name="${session_name}-old"
+  tmux rename-session $tmp_session_name
+  tmux new-session -s $session_name -d
+  local window_id
+  for window_id in $(tmux list-windows -F '#I'); do
+    tmux move-window -s $tmp_session_name:$window_id -t $session_name
+  done
+  tmux kill-window -t $session_name:0
+  tmux switch-client -t $session_name
+}
+alias here=change_tmux_pwd
