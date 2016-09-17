5b5960c56f85e2879e19157b2263c0fc1f695662
Nate Bosch <nbosch1@gmail.com>
2016-09-16 21:38:34 -0700

    Native zsh git status for prompt
    Rewrite zsh prompt to not depend on an external python script.

--- zshrc.d/git.zsh
+++ zshrc.d/git.zsh
@@ -1,54 +1,62 @@
 # Fancy git prompt
 function git-status() {
-    GIT_STATUS=""
-    ZSH_THEME_GIT_PROMPT_PREFIX="("
-    ZSH_THEME_GIT_PROMPT_SUFFIX=")"
-    ZSH_THEME_GIT_PROMPT_SEPARATOR="|"
-    ZSH_THEME_GIT_PROMPT_BRANCH="%{$fg_bold[magenta]%}"
-    ZSH_THEME_GIT_PROMPT_STAGED="%{$fg[red]%}."
-    ZSH_THEME_GIT_PROMPT_CONFLICTS="%{$fg[red]%}."
-    ZSH_THEME_GIT_PROMPT_CHANGED="%{$fg[cyan]%}."
-    ZSH_THEME_GIT_PROMPT_REMOTE=""
-    ZSH_THEME_GIT_PROMPT_UNTRACKED="."
-    ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg_bold[green]%}."
+  GIT_STATUS=""
+  git status &>/dev/null || return
 
-    local gitstatus="$ZDOTDIR/.gitstatus/gitstatus.py"
-    if [ -x $gitstatus ]; then
-        _GIT_GIT_STATUS=`python ${gitstatus}`
-        __CURRENT_GIT_GIT_STATUS=("${(@f)_GIT_GIT_STATUS}")
-        GIT_BRANCH=$__CURRENT_GIT_GIT_STATUS[1]
-        GIT_REMOTE=$__CURRENT_GIT_GIT_STATUS[2]
-        GIT_STAGED=$__CURRENT_GIT_GIT_STATUS[3]
-        GIT_CONFLICTS=$__CURRENT_GIT_GIT_STATUS[4]
-        GIT_CHANGED=$__CURRENT_GIT_GIT_STATUS[5]
-        GIT_UNTRACKED=$__CURRENT_GIT_GIT_STATUS[6]
-        GIT_CLEAN=$__CURRENT_GIT_GIT_STATUS[7]
-        if [ -n "$__CURRENT_GIT_GIT_STATUS" ]; then
-        GIT_STATUS="($GIT_BRANCH"
-        GIT_STATUS="$ZSH_THEME_GIT_PROMPT_PREFIX$ZSH_THEME_GIT_PROMPT_BRANCH$GIT_BRANCH%{${reset_color}%}"
-        if [ -n "$GIT_REMOTE" ]; then
-            GIT_STATUS="$GIT_STATUS$ZSH_THEME_GIT_PROMPT_REMOTE$GIT_REMOTE%{${reset_color}%}"
-        fi
-        GIT_STATUS="$GIT_STATUS$ZSH_THEME_GIT_PROMPT_SEPARATOR"
-        if [ "$GIT_STAGED" -ne "0" ]; then
-            GIT_STATUS="$GIT_STATUS$ZSH_THEME_GIT_PROMPT_STAGED$GIT_STAGED%{${reset_color}%}"
-        fi
-        if [ "$GIT_CONFLICTS" -ne "0" ]; then
-            GIT_STATUS="$GIT_STATUS$ZSH_THEME_GIT_PROMPT_CONFLICTS$GIT_CONFLICTS%{${reset_color}%}"
-        fi
-        if [ "$GIT_CHANGED" -ne "0" ]; then
-            GIT_STATUS="$GIT_STATUS$ZSH_THEME_GIT_PROMPT_CHANGED$GIT_CHANGED%{${reset_color}%}"
-        fi
-        if [ "$GIT_UNTRACKED" -ne "0" ]; then
-            GIT_STATUS="$GIT_STATUS$ZSH_THEME_GIT_PROMPT_UNTRACKED%{${reset_color}%}"
-        fi
-        if [ "$GIT_CLEAN" -eq "1" ]; then
-            GIT_STATUS="$GIT_STATUS$ZSH_THEME_GIT_PROMPT_CLEAN"
-        fi
-            GIT_STATUS="$GIT_STATUS%{${reset_color}%}$ZSH_THEME_GIT_PROMPT_SUFFIX"
-        fi
-        unset __CURRENT_GIT_GIT_STATUS
-    fi
+  local PROMPT_PREFIX="("
+  local PROMPT_SUFFIX=")"
+  local PROMPT_SEPARATOR="|"
+  local PROMPT_BRANCH="%{$fg_bold[magenta]%}"
+  local PROMPT_STAGED="%{$fg[red]%}"
+  local PROMPT_UNSTAGED="%{$fg[cyan]%}"
+  local PROMPT_CONFLICTS="%{$fg[red]%}!"
+  local PROMPT_AHEAD="%{$fg[white]%}+"
+  local PROMPT_BEHIND="%{$fg[white]%}-"
+  local PROMPT_UNTRACKED="%{$fg[red]%}."
+  local PROMPT_CLEAN="%{$fg_bold[green]%}."
+
+  local UPSTREAM=$(git rev-parse --abbrev-ref --symbolic-full-name @{u})
+
+  local GIT_AHEAD=$(git rev-list --left-only --count $GIT_BRANCH...$UPSTREAM)
+  local GIT_BEHIND=$(git rev-list --right-only --count $GIT_BRANCH...$UPSTREAM)
+  local GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+  local GIT_CONFLICTS=$(git ls-files --unmerged | cut -f2 | sort -u | wc -l)
+  local GIT_STAGED=$(git diff --cached --numstat | wc -l)
+  local GIT_UNSTAGED=$(git diff --numstat | wc -l)
+  local GIT_UNTRACKED=$(git ls-files -o --exclude-standard | wc -l)
+
+  GIT_STATUS="($GIT_BRANCH"
+  GIT_STATUS="$PROMPT_PREFIX$PROMPT_BRANCH$GIT_BRANCH%{${reset_color}%}"
+  GIT_STATUS="$GIT_STATUS$PROMPT_SEPARATOR"
+  GIT_CLEAN=true
+  if [ "$GIT_AHEAD" -ne "0" ]; then
+    GIT_STATUS="$GIT_STATUS$PROMPT_AHEAD$GIT_AHEAD%{${reset_color}%}"
+    GIT_CLEAN=false
+  fi
+  if [ "$GIT_BEHIND" -ne "0" ]; then
+    GIT_STATUS="$GIT_STATUS$PROMPT_BEHIND$GIT_BEHIND%{${reset_color}%}"
+    GIT_CLEAN=false
+  fi
+  if [ "$GIT_CONFLICTS" -ne "0" ]; then
+    GIT_STATUS="$GIT_STATUS$PROMPT_CONFLICTS$GIT_CONFLICTS%{${reset_color}%}"
+    GIT_CLEAN=false
+  fi
+  if [ "$GIT_STAGED" -ne "0" ]; then
+    GIT_STATUS="$GIT_STATUS$PROMPT_STAGED$GIT_STAGED%{${reset_color}%}"
+    GIT_CLEAN=false
+  fi
+  if [ "$GIT_UNSTAGED" -ne "0" ]; then
+    GIT_STATUS="$GIT_STATUS$PROMPT_UNSTAGED$GIT_UNSTAGED%{${reset_color}%}"
+    GIT_CLEAN=false
+  fi
+  if [ "$GIT_UNTRACKED" -ne "0" ]; then
+    GIT_STATUS="$GIT_STATUS$PROMPT_UNTRACKED%{${reset_color}%}"
+    GIT_CLEAN=false
+  fi
+  if $GIT_CLEAN; then
+    GIT_STATUS="$GIT_STATUS$PROMPT_CLEAN"
+  fi
+  GIT_STATUS="$GIT_STATUS%{${reset_color}%}$PROMPT_SUFFIX"
 }
 
 autoload add-zsh-hook
