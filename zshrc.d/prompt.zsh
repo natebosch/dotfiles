b589928e4548fc0b955ec2e1b1c7a01ec868a12a
Nate Bosch <nbosch1@gmail.com>
2016-12-09 20:23:42 -0800

    Change prompt path to orange
    `ag` prints paths in green and the prompt path blends in too much, pick
    a different color rather than try to figure out how to configure ag.
    
    - Switch PATH_COLOR to a light orange
    - Use %F{color} instead of ${fg[color]} since it support 256 colors
    - Use %f instead of %{$reset_color%} or ${fg[defuault]} since it's
      shorter

--- zshrc.d/prompt.zsh
+++ zshrc.d/prompt.zsh
@@ -1,12 +1,12 @@
 local PROMPT_COLOR=cyan
-local PATH_COLOR=green
+local PATH_COLOR=172
 local TIME_COLOR=blue
 autoload colors
 colors
 setopt prompt_subst
-local smiley="%(?,%{$fg[green]%}☺%{$reset_color%},%{$fg[red]%}☹%{$reset_color%})"
-local working_dir="%{${fg[$PATH_COLOR]}%}%B%(7~,.../,)%6~%b%{${fg[default]}%}"
+local smiley="%(?,%F{green}☺%f,%F{red}☹%f)"
+local working_dir="%F{$PATH_COLOR}%B%(7~,.../,)%6~%b%f"
 PROMPT="\$working_dir \$GIT_STATUS
-%b[%{${fg[$TIME_COLOR]}%}%B%T%b]\$smiley%{${fg[default]}%}  "
+%b[%F{$TIME_COLOR%}%B%T%b]\$smiley%f  "
 
 RPROMPT=""
