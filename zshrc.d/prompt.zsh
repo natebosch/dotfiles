56e0c92fa69cb69c76cd258852eff39c8a08a123
Nate Bosch <nbosch1@gmail.com>
2017-01-25 22:02:27 -0800

    Tweak prompt characters
    Use characters that have glyphs in source-code-pro

--- zshrc.d/prompt.zsh
+++ zshrc.d/prompt.zsh
@@ -3,9 +3,9 @@ local TIME_COLOR=33
 autoload colors
 colors
 setopt prompt_subst
-local smiley="%(?,%F{green}☺%f,%F{red}☹%f)"
+local success="%B%(?,%F{green}»,%F{red}⁉)%b%f"
 local working_dir="%F{$PATH_COLOR}%B%(7~,.../,)%6~%b%f"
 PROMPT="\$working_dir \$GIT_STATUS
-%b[%F{$TIME_COLOR%}%B%T%b]\$smiley%f  "
+%b[%F{$TIME_COLOR%}%B%T%b]\$success%f  "
 
 RPROMPT=""
