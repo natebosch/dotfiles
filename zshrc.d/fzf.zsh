858e461c6e4dc9ad87769cd586e83516226a956a
Nate Bosch <nbosch1@gmail.com>
2017-12-06 13:58:42 -0800

    Add default_find_files fallback command
    This allows a local `find_files` to also fallback rather than fail.

--- zshrc.d/fzf.zsh
+++ zshrc.d/fzf.zsh
@@ -1,17 +1,7 @@
 export FZF_DEFAULT_OPTS="--exact"
 
-if (( $+commands[ag] )) ; then
-  export FZF_DEFAULT_COMMAND="find_files || ag -l --nocolor --nogroup --depth 10 -g ''"
-fi
-
-# ag doesn't work with ctrl_t fzf
-defaultFind="\
-command find -L . \\( -path '*/\\.*' -o -fstype 'dev' -o -fstype 'proc' \\) -prune \
-      -o -type f -print \
-      -o -type d -print \
-      -o -type l -print 2> /dev/null | sed 1d | cut -b3-"
-export FZF_CTRL_T_COMMAND="find_files || $defaultFind"
-unset defaultFind
+export FZF_DEFAULT_COMMAND="find_files || default_find_files"
+export FZF_CTRL_T_COMMAND="find_files || default_find_files"
 
 # Fallbacks if fzf isn't available
 if [ ! -f ~/.fzf.zsh ]
