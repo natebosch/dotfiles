3c7a2239aef036b9534d69fa177a3adb572883b3
Nate Bosch <nbosch1@gmail.com>
2019-04-11 16:50:32 -0700

    Disable homebrew attempt to use github API
    Also invert mac check condition with a bail out to reduce nesting.

--- zshrc.d/mac.zsh
+++ zshrc.d/mac.zsh
@@ -1,9 +1,12 @@
-if [[ "$(uname)" == "Darwin" ]]; then
-  # Brew should be early in the path
-  path=($DOTDIR/.brew/bin(N-/) $path)
-  manpath=($DOTDIR/.brew/share/man(N-/) $manpath)
-  ld_library_path=($DOTDIR/.brew/lib(N-/) $ld_library_path)
-
-  # The `ls` on mac is missing `--color=auto`
-  alias ls='ls -F -G'
+if [[ "$(uname)" != "Darwin" ]]; then
+  return 0
 fi
+# Brew should be early in the path
+path=($DOTDIR/.brew/bin(N-/) $path)
+manpath=($DOTDIR/.brew/share/man(N-/) $manpath)
+ld_library_path=($DOTDIR/.brew/lib(N-/) $ld_library_path)
+
+export HOMEBREW_NO_GITHUB_API=true
+
+# The `ls` on mac is missing `--color=auto`
+alias ls='ls -F -G'
