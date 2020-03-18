95d0318d1d7f4bff94d57063b1002fa644389c2d
Nate Bosch <nbosch1@gmail.com>
2020-03-17 20:40:54 -0700

    Add projects in session_finder
    Include projects in the list of sessions. Add a postfix `P` or `*` to
    distinguish projects from sessions. If a session already exists for a
    project it is listed as a session, not a project. Move the directory
    finding logic from the `project` command to `session_finder` for use
    when opening a project without an existing session.
    
    Remove the `project` script and bind-key.
    
    - Use `mapfile` instead of the longer `read` approach.
    - Use `tmux has-session` over checking the array in bash.
    - When there are no sessions, add default "0".
    - Replace `.` for projects with `_` to make a valid tmux session name.
    - `cd` to home directory for session "0". Remove from tmux bind-key.
    - Add `set -e` to fail fast.
    - Fix `shellcheck` warnings.
    - Remove compdef for removed project sctipt.

--- zshrc.d/functions.zsh
+++ zshrc.d/functions.zsh
@@ -31,6 +31,3 @@ watchfile() {
 }
 
 say() { if [[ "${1}" =~ -[a-z]{2} ]]; then local lang=${1#-}; local text="${*#$1}"; else local lang=${LANG%_*}; local text="$*";fi; mplayer "http://translate.google.com/translate_tts?ie=UTF-8&tl=${lang}&q=${text}" &> /dev/null ; }
-
-compdef '_arguments "1:project name:($(ls ~/projects))"' project
-
