6532a4beb58563d5e8cc714968de522987ffa852
Nate Bosch <nbosch1@gmail.com>
2021-02-16 22:37:44 -0800

    Simplify fshow using an execute fzf action
    Replace the separate script calling `fzc` in a loop with a `--bind`
    argument mapping to an `execute` action to run `git show`. This avoids
    the need to manually handle passing the query back since fzf retains it.
    
    Since there is no longer a need for the -p and -q options originally
    used by `fshow`, simplify all the way down to a single chain of piped
    commands and drop arg parsing altogether. Arguments can be forwarded
    directly to the `git log` command now, without an extra `--`.
    
    - Simplify the preview argument using the `{1}` placeholder instead of
      `{}` to avoid `cut` in the preview command.
    - Switch ctrl-d and ctrl-u keybinds to scroll the preview window. Remove
      those keybinds from the default options.
    - Add a bind to open the full `git show` for the selected commit.

--- zshrc.d/fzf.zsh
+++ zshrc.d/fzf.zsh
@@ -1,5 +1,4 @@
-export FZF_DEFAULT_OPTS="--exact --inline-info \
-  --bind=ctrl-d:half-page-down --bind=ctrl-u:half-page-up"
+export FZF_DEFAULT_OPTS="--exact --inline-info"
 
 export FZF_DEFAULT_COMMAND="find_files"
 export FZF_CTRL_T_COMMAND="find_files"
